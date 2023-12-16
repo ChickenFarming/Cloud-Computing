3515246c84501ba4f1194d58650a14993a860ab5
SyahrinNanda <syahrinnanda@gmail.com>
2023-12-16 13:53:34 +0800

    update database.js add gcloudeignore add app.yaml

--- App/src/config/database.js
+++ App/src/config/database.js
@@ -4,8 +4,9 @@ const mysql = require("mysql2");
 const dbPool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
-  password: process.env.DB_PASS,
+  // password: process.env.DB_PASS,
+  password: "chickfarm123",
   database: process.env.DB_NAME,
 });
-
+// console.log(dbPool);
 module.exports = dbPool.promise();
